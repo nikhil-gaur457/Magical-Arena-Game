9b2a44790ec9dba687ebbacf779be6776b313b13
nikhilg_23 <nikhilg@easyecom.io>
2024-11-12 23:20:05 +0530

    Create Player class with attributes and methods for health, strength, and attack

---
+++ src/main/Player.java
@@ -0,0 +1,44 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Nikhil gaur (nikhil-gaur457)
+ *   Date: 12-11-2024
+ *   Time: 23:00
+ *   File: Player.java
+ */
+
+package main;
+
+class Player {
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+}
